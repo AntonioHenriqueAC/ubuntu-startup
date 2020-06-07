fdd09ce054958122bc740751ce8f6e49356af73b
Antonio Cardoso <antoniohenriqueac@gmail.com>
2020-06-07 20:22:22 -0300

    add edit theme and .zshrc file

---
+++ .zshrcNew.sh
@@ -0,0 +1,169 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export AWS_SDK_LOAD_CONFIG=1 
+
+# Export variable
+export DEVELOPMENT="$HOME/projects/src"
+
+# Create Aliases
+
+alias dev='cd $DEVELOPMENT'
+alias r='rm -rf '
+alias cd2='cd ../..'
+alias cd3='cd ../../..'
+alias bashrc='vim ~/.bashrc'
+alias ohmy='cd $HOME/.oh-my-zsh'
+alias zshrc='vim ~/.zshrc'
+alias la='ls -lha'
+alias ll='ls -lh'
+alias lk='ls -lh | awk '"'"'{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf("%0o ",k);print}'"'"''
+
+alias gps='git push'
+alias gpl='git pull'
+alias gplom='git pull origin master'
+alias gckm='git checkout master'
+alias gck='git checkout'
+alias gs='git status -s'
+alias gcmt='git commit -m'
+alias ga='git add'
+
+alias pm22='cd $DEVELOPMENT && pm2 start projects.json'
+alias pm23='cd $DEVELOPMENT && pm2 start projects.json && sudo timeout 3 sleep 5 || pm2 log'
+alias pm2l='pm2 log'
+alias pm2s='pm2 status'
+alias pm2r='pm2 reload'
+alias pm2ra='pm2 reload all'
+
+alias nvm4='nvm use 4'
+
+# cowsay -f $(ls /usr/share/cowsay/ | shuf -n1) "Opa, bão?"
+
+
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="powerlevel10k/powerlevel10k"
+#[[ ! -f /tmp/zsh/theme.zsh ]] || source /tmp/zsh/theme.zsh
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to automatically update without prompting.
+# DISABLE_UPDATE_PROMPT="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS=true
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+  aws
+  bgnotify
+  docker
+  systemd
+  npm
+  npx
+  nvm
+  docker-compose
+  #gcloud
+  git
+  gradle
+  history
+  kubectl
+  node
+)
+
+source $ZSH/oh-my-zsh.sh
+
+source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/zsh-completions/zsh-completions.plugin.zsh
+#source $HOME/.oh-my-zsh/powerlevel10k/powerlevel10k.zsh-theme
+ZSH_COMPDUMP="$HOME/.cache/.zcompdump-${ZSH_VERSION}"
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- startup.sh
+++ startup.sh
@@ -1,10 +1,24 @@
 sudo apt-get update -y
 
+eval echo ~$USER
+
 echo 'installing curl' 
 sudo apt install curl -y
 
 echo 'installing git' 
 sudo apt install git -y
+
+echo 'Installing vim'
+sudo apt install vim -y
+clear
+
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, no problem. :) Let's move on!"
+fi
 
 echo "What name do you want to use in GIT user.name?"
 echo "For example, mine will be \"Antonio Cardoso\""
@@ -18,18 +32,15 @@ read git_config_user_email
 git config --global user.email $git_config_user_email
 clear
 
-echo "Can I set VIM as your default GIT editor for you? (y/n)"
-read git_core_editor_to_vim
-if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
-	git config --global core.editor vim
-else
-	echo "Okay, no problem. :) Let's move on!"
-fi
+echo 'Installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
 
 echo "Generating a SSH Key"
 ssh-keygen -t rsa -b 4096 -C $git_config_user_email
-ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+eval `ssh-agent -s`
+ssh-add
+clip.exe < ~/.ssh/id_rsa.pub
+echo "Paste your SSH key in your Github (github.com/settings/keys) -> (ctrl + v) !!"
 
 echo 'Installing zsh'
 sudo apt-get install zsh -y
@@ -38,17 +49,24 @@ chsh -s /bin/zsh
 
 echo 'Installing oh-my-zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo 'Installing tool to handle clipboard via CLI'
-sudo apt-get install xclip -y
-
 export alias pbcopy='xclip -selection clipboard'
 export alias pbpaste='xclip -selection clipboard -o'
 source ~/.zshrc
 
-echo 'Installing vim'
-sudo apt install vim -y
-clear
+mv .zshrcNew $HOME/.zshrc
+
+echo 'installing autosuggestions, zsh-completions and zsh-syntax-highlighting' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+git clone git://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+echo "source ~/.zsh/zsh-completions/zsh-completions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing theme'
+sudo apt install fonts-firacode -y
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo 'Installing Visual Studio Code'
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -59,15 +77,58 @@ sudo apt-get update
 sudo apt-get install code -y # or code-insiders
 
 echo 'Installing extensions'
+code --install-extension alexandersage.angular1-code-snippets
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension hookyqr.beautify
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension ms-vscode.cpptools
+code --install-extension vikas.code-navigation
+code --install-extension formulahendry.code-runner
+code --install-extension naumovs.color-highlight
+code --install-extension luis.console-log
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension redhat.fabric8-analytics
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension mikestead.dotenv
+code --install-extension geeebe.duplicate
+code --install-extension dhide.easy-php-error-logger
+code --install-extension editorconfig.editorconfig
+code --install-extension digitalbrainstem.javascript-ejs-support
+code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dracula-theme.theme-dracula
-code --install-extension esbenp.prettier-vscode
-code --install-extension foxundermoon.shell-format
-code --install-extension pmneo.tsimporter
 code --install-extension waderyan.gitblame
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension spywhere.guides
+code --install-extension ecmel.vscode-html-css
+code --install-extension wix.vscode-import-cost
+code --install-extension zignd.html-css-class-completion
+code --install-extension nickdemayo.vscode-json-editor
+code --install-extension ritwickdey.live-sass
+code --install-extension ritwickdey.liveserver
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension yzhang.markdown-all-in-one
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension afractal.node-essentials
+code --install-extension eg2.vscode-npm-script
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension felixfbecker.php-intellisense
+code --install-extension alefragnani.project-manager
+code --install-extension ms-python.python
+code --install-extension tht13.python
+code --install-extension esbenp.prettier-vscode
+code --install-extension wallabyjs.quokka-vscode
+code --install-extension ms-vscode.references-view
+code --install-extension jasonnutter.search-node-modules
+code --install-extension shan.code-settings-sync
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension funkyremi.vscode-google-translate
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension shyykoserhiy.vscode-spotify
+code --install-extension dotjoshjohnson.xml
+code --install-extension redhat.vscode-yaml
 
 echo 'Installing spotify' 
 snap install spotify
@@ -94,20 +155,6 @@ nvm install 12
 nvm alias default 12
 node --version
 npm --version
-
-echo 'installing autosuggestions, zsh-completions and zsh-syntax-highlighting' 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-git clone git://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
-echo "source ~/.zsh/zsh-completions/zsh-completions.zsh" >> ~/.zshrc
-source ~/.zshrc
-
-echo 'installing theme'
-sudo apt install fonts-firacode -y
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
 
 echo 'installing slack' 
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
